c974d16e814fe0c8092dd2397779abba2f89e344
Régis Teixeira <regismarcondes2016@gmail.com>
2022-10-03 00:43:58 -0300

    Ajustes

--- src/controllers/EventoController.js
+++ src/controllers/EventoController.js
@@ -2,29 +2,37 @@ const ModelEvento = require('../models/evento');
 
 module.exports =
 {
-    async List(res) {
-         res.header("Access-Control-Allow-Origin", "*");
+    /**
+     * 
+     * @param {Express.Request} _ 
+     * @param {Express.Response} res 
+     * @return {ModelEvento[]}
+     * 
+     */
+    async List(_, res) {
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         try {
             const eventos = await ModelEvento.findAll();
             return res.json(eventos);
 
         } catch (erro) {
-            return console.error("Erro na List : ", erro);
+            console.error("Erro na List : ", erro);
+            throw new Error('Error during listing events');
         }
     },
 
     async Create(req, res) {
-              //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-        
+        //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+        res.header("Access-Control-Allow-Origin", "*");
+        //Quais são os métodos que a conexão pode realizar na API
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+
         try {
             const eventos = await ModelEvento.create(
                 {
-                   //Codigo: req.body.Codigo, // Comentado para gerar automatico
-                    NomeEvento: req.body.NomeEvento, 
+                    //Codigo: req.body.Codigo, // Comentado para gerar automatico
+                    NomeEvento: req.body.NomeEvento,
                     OngResponsavel: req.body.OngResponsavel,
                     Descricao: req.body.Descricao,
                     DataEvento: req.body.DataEvento,
