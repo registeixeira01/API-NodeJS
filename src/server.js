f92ea66f000128bc2e7094791495e8814e077090
Régis Teixeira <regismarcondes2016@gmail.com>
2022-10-14 00:06:42 -0300

    Adicionando funções de usuários

--- src/server.js
+++ src/server.js
@@ -1,7 +1,89 @@
 const express = require('express');
 const api = express();
+const app = express();
 const routes = require('./router');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { eAdmin } = require('./middlewares/auth');
+const User = require('./models/User');
+
+app.use(express.json());
+
+app.get('/', eAdmin, async (req, res) => {
+    await User.findAll({
+        attributes: ['id', 'name', 'email'],
+        order: [['id', "DESC"]]
+    })
+    .then((users) => {
+        return res.json({
+            erro: false,
+            users,
+            id_usuario_logado: req.userId
+        });
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Nenhum usuário encontrado!"
+        });
+    });    
+});
+
+app.post('/cadastrar', async (req, res) => {
+    var dados = req.body;
+
+    dados.password = await bcrypt.hash(dados.password, 8);
+
+    await User.create(dados)
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: "Usuário cadastrado com sucesso!"
+        });
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário não cadastrado com sucesso!"
+        });
+    });    
+});
+
+app.post('/login', async (req, res) => {
+
+    const user = await User.findOne({
+        attributes: ['id', 'name', 'email', 'password'],
+        where: {
+            email: req.body.email
+        }
+    });
+
+    if(user === null){
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário ou a senha incorreta!"
+        });
+    }
+
+    if(!(await bcrypt.compare(req.body.password, user.password))){
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário ou a senha incorreta!"
+        });
+    }
+
+    var token = jwt.sign({id: user.id}, "D62ST92Y7A6V7K5C6W9ZU6W8KS3", {
+        expiresIn: 600 //10 min
+        //expiresIn: 60 //1 min
+        //expiresIn: '7d' // 7 dia
+    });
+
+    return res.json({
+        erro: false,
+        mensagem: "Login realizado com sucesso!",
+        token
+    });
+});
 
 api.use(cors());
 api.use(express.json());
@@ -10,3 +92,4 @@ api.use(routes);
 
 
 api.listen(4200);
+app.listen(4100);
